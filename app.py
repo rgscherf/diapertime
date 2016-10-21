61429335b5b523cbea3ae3a5e1b188698942b5cb
rgscherf <rgscherf@gmail.com>
2016-10-20 22:48:30 -0400

    scaffolding up and running

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
+db = SQLAlchemy(app)
 
 
 @app.route("/")
 def hello():
-    return "hello world!"
+    return render_template("index.html", text="I'm here!")
 
 if __name__ == "__main__":
     app.run()
