93e940e45ab2fbae01cfb20c000cac7fc6e63ed8
rgscherf <rgscherf@gmail.com>
2016-11-07 14:10:24 -0500

    mongo

--- app.py
+++ app.py
@@ -1,67 +1,58 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-import os
+from pymongo import MongoClient
+from bson.json_util import dumps
 import datetime
-import json
+import random
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-db = SQLAlchemy(app)
 
 
-class DiaperTime(db.Model):
-    __tablename__ = "diaper_time"
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
-    skipped_previous_feed = db.Column(db.Boolean)
-    poop = db.Column(db.Integer)
-    pee = db.Column(db.Boolean)
-    breast_feed = db.Column(db.Integer)
-    bottle_feed = db.Column(db.Integer)
-    attended_at = db.Column(db.DateTime)
-    slept_at = db.Column(db.DateTime)
+client = MongoClient()
+client = MongoClient('localhost', 27017)
 
-    def __init__(self, s, p, pee, br, bo, att, sle):
-        poops = {"none": 0,
-                 "scant": 1,
-                 "normal": 2,
-                 "heavy": 3}
+db = client['test_database']
+collection = db['test_collection']
+
+
+class DiaperTime():
+
+    def __init__(self, att, s, p, pee, br, bo, sle):
+        self.attended_at = att
         self.skipped_previous_feed = s
-        self.poop = poops[p]
+        self.poop = p
         self.pee = pee
         self.breast_feed = br
         self.bottle_feed = bo
-        self.attended_at = att
         self.slept_at = sle
 
     def to_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        return {"attendedAt": self.attended_at,
+                "skippedPrevious": self.skipped_previous_feed,
+                "poop": self.poop,
+                "pee": self.pee,
+                "breastFeed": self.breast_feed,
+                "bottleFeed": self.bottle_feed,
+                "sleptAt": self.slept_at}
 
 
-def json_time(o):
-    if isinstance(o, datetime.datetime):
-        return o.isoformat()
-    else:
-        raise TypeError
-
-
-@app.route("/dummy")
-def dummy():
-    dum1 = DiaperTime(False, "none", True, 20, 20,
-                      datetime.datetime.now(), datetime.datetime.now())
-    dum2 = DiaperTime(False, "heavy", True, 0, 60,
-                      datetime.datetime.now(), datetime.datetime.now())
-    db.session.add(dum1)
-    db.session.add(dum2)
-    db.session.commit()
-    return "hello"
+@app.route("/genrandom")
+def gen():
+    for i in range(10):
+        a = DiaperTime(datetime.datetime.now(),
+                       random.random() > 0.5,
+                       random.randrange(0, 3),
+                       random.random() > 0.5,
+                       random.randrange(20),
+                       random.randrange(60, 120),
+                       datetime.datetime.now())
+        collection.insert_one(a.to_dict())
+    return "operation successful"
 
 
 @app.route("/")
 def hello():
-    all_events = [e.to_dict() for e in DiaperTime.query.all()]
-    jsonified_events = json.dumps(all_events, sort_keys=True, default=json_time)
+    all_events = [c for c in collection.find()]
+    jsonified_events = dumps(all_events)
     return jsonified_events
     # return render_template("index.html", data=jsonified_events)
 
