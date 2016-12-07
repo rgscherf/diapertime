71ae409fdcb575c4259906d5dab16736085bc65f
rgscherf <rgscherf@gmail.com>
2016-12-07 05:52:00 -0500

    add mongo connection in clj-diapertime.db

---
+++ src/clj/clj_diaper/db.clj
@@ -0,0 +1,23 @@
+(ns clj-diaper.db
+  (:require [monger.core :as mcore]
+            [monger.db :as mdb]
+            [monger.collection :as mcoll]))
+  ; (:import [com.mongodb MongoOptions ServerAddress]))
+
+(defn db-connect
+  []
+  (let [conn (mcore/connect)
+        db (mcore/get-db conn "diapertime")]
+    {:conn conn :db db}))
+
+(defn db-disconnect
+  [connection]
+  (mcore/disconnect connection))
+  
+(let [{:keys [conn db]} (db-connect)]
+  (def connection conn)
+  (def database db))
+
+(def user-collection "user-collection")
+(def baby-collection "baby-collection")
+(def event-collection "event-collection")
---
+++ src/clj/clj_diaper/util.clj
@@ -0,0 +1,1 @@
+(ns clj-diaper.util)
